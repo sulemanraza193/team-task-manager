c7ec18fe41ea554593cffe96f13e4ed0a4439600
Suleman Raza <sulemanraza193@gmail.com>
2026-06-06 22:45:42 +0500

    Update baseURL in axios configuration

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // If VITE_API_URL exists, use it. Otherwise, fallback to localhost for development
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
-  withCredentials: true,
-  console.log("Current API URL:", import.meta.env.VITE_API_URL);
+  baseURL: import.meta.env.VITE_API_URL || '/',
+  withCredentials: true,
 });
 
 export default api;
